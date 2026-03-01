0f676631f7266f11e4fadb73f1a271e2d1b77131
codex20260221 <accounts+codex20260221@michaelmawhinney.com>
2026-03-01 17:03:26 -0500

    Refactor Cryptex to versioned base64url envelope with legacy decrypt support

--- src/Cryptex.php
+++ src/Cryptex.php
@@ -1,142 +1,228 @@
 <?php
+
+declare(strict_types=1);
+
 namespace cryptex;
 
 /**
  * Cryptex performs 2-way authenticated encryption using XChaCha20 + Poly1305.
  *
  * This class leverages the Sodium crypto library, added to PHP in version 7.2.
- * A salt value of length SODIUM_CRYPTO_PWHASH_SALTBYTES is required and should
- * be randomly generated with the included generateSalt() function or another
- * secure function like random_bytes().
  *
  * @category Encryption/Decryption
  * @package Cryptex
  * @author Michael Mawhinney
  * @copyright 2023
  * @license https://opensource.org/licenses/MIT/ MIT
- * @version 4.0.0
+ * @version 5.0.0
  */
 final class Cryptex
 {
     /**
-     * @var int  Required length of the nonce value
+     * @var int Required length of the nonce value.
      */
     private const NONCE_LENGTH = \SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_NPUBBYTES;
 
     /**
-     * @var int  Required length of the salt value
+     * @var int Required length of the salt value.
      */
     private const SALT_LENGTH = \SODIUM_CRYPTO_PWHASH_SALTBYTES;
 
     /**
+     * @var int Auth tag length for XChaCha20-Poly1305.
+     */
+    private const TAG_LENGTH = \SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_ABYTES;
+
+    /**
+     * @var int Envelope version byte for base64url payloads.
+     */
+    private const ENVELOPE_VERSION_V1 = 0x01;
+
+    /**
      * Encrypts data using XChaCha20 + Poly1305 (from the Sodium crypto library).
+     *
+     * Output format (v1, base64url): [version byte][salt][nonce][ciphertext+tag]
      *
      * @param string $plaintext Unencrypted data.
      * @param string $key Encryption key.
      * @param string $salt Salt value of length SODIUM_CRYPTO_PWHASH_SALTBYTES.
-     * @return string Encrypted data (hex-encoded).
+     * @return string Encrypted data (base64url encoded envelope).
      *
      * @throws EncryptionException If the data encryption fails.
      */
     public static function encrypt(string $plaintext, string $key, string $salt): string
     {
-        try {
-            // Generate a derived binary key
-            $derivedKey = self::generateDerivedKey($key, $salt);
+        $derivedKey = null;
+        $nonce = null;
 
-            // Generate a nonce value of the correct size
+        try {
+            $derivedKey = self::generateDerivedKey($key, $salt);
             $nonce = random_bytes(self::NONCE_LENGTH);
 
-            // Encrypt the data
             $encryptedData = sodium_crypto_aead_xchacha20poly1305_ietf_encrypt(
                 $plaintext,
                 '',
                 $nonce,
                 $derivedKey
             );
+
             if ($encryptedData === false) {
                 throw new EncryptionException('Failed to encrypt the data');
             }
 
-            // Prepend the nonce, and hex encode
-            $ciphertext = sodium_bin2hex($nonce . $encryptedData);
+            $envelope = chr(self::ENVELOPE_VERSION_V1) . $salt . $nonce . $encryptedData;
 
-            // Return the encrypted data
-            return $ciphertext;
-        } catch (Exception $e) {
-            // Rethrow the exception
-            throw $e;
+            return sodium_bin2base64($envelope, \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING);
+        } catch (\Throwable $e) {
+            if ($e instanceof EncryptionException || $e instanceof SaltLengthException) {
+                throw $e;
+            }
+
+            throw new EncryptionException('Encryption failed', previous: $e);
         } finally {
-            // Wipe sensitive data
-            sodium_memzero($plaintext);
-            sodium_memzero($key);
-            sodium_memzero($salt);
-            sodium_memzero($derivedKey);
+            self::wipeString($derivedKey);
+            self::wipeString($nonce);
         }
     }
 
     /**
      * Authenticates and decrypts data encrypted by Cryptex (XChaCha20+Poly1305).
      *
+     * Supports both the v1 base64url envelope and the legacy hex format.
+     *
      * @param string $ciphertext Encrypted data.
      * @param string $key Encryption key.
-     * @param string $salt Salt value.
+     * @param string $salt Salt value for legacy hex payloads.
      * @return string Unencrypted data.
      *
-     * @throws NonceLengthException If the decoded data is not the expected length.
-     * @throws DecryptionException If the data decryption fails.
+     * @throws NonceLengthException If payload contents are not the expected length.
+     * @throws DecryptionException If data decryption fails.
      */
     public static function decrypt(string $ciphertext, string $key, string $salt): string
     {
+        if (self::looksLikeLegacyHex($ciphertext)) {
+            return self::decryptLegacyHex($ciphertext, $key, $salt);
+        }
+
+        $derivedKey = null;
+        $decoded = null;
+        $nonce = null;
+        $ciphertextBody = null;
+        $envelopeSalt = null;
+
         try {
-            // Generate a derived binary key
-            $derivedKey = self::generateDerivedKey($key, $salt);
+            $decoded = sodium_base642bin(
+                $ciphertext,
+                \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING,
+                ''
+            );
 
-            // Hex decode
-            $decoded = sodium_hex2bin($ciphertext);
-
-            // Check the decoded length
-            if (strlen($decoded) < self::NONCE_LENGTH) {
+            $minimumLength = 1 + self::SALT_LENGTH + self::NONCE_LENGTH + self::TAG_LENGTH;
+            if (strlen($decoded) < $minimumLength) {
                 throw new NonceLengthException('Decoded data is not the expected length');
             }
 
-            // Get the nonce value from the decoded data
-            $nonce = mb_substr(
-                $decoded,
-                0,
-                self::NONCE_LENGTH,
-                '8bit'
-            );
+            $version = ord($decoded[0]);
+            if ($version !== self::ENVELOPE_VERSION_V1) {
+                throw new DecryptionException('Unsupported ciphertext version');
+            }
 
-            // Get the ciphertext from the decoded data
-            $ciphertext = mb_substr(
-                $decoded,
-                self::NONCE_LENGTH,
-                null,
-                '8bit'
-            );
+            $offset = 1;
+            $envelopeSalt = substr($decoded, $offset, self::SALT_LENGTH);
+            $offset += self::SALT_LENGTH;
 
-            // Decrypt the data
+            $nonce = substr($decoded, $offset, self::NONCE_LENGTH);
+            $offset += self::NONCE_LENGTH;
+
+            $ciphertextBody = substr($decoded, $offset);
+            if (strlen($ciphertextBody) < self::TAG_LENGTH) {
+                throw new NonceLengthException('Ciphertext is not the expected length');
+            }
+
+            $derivedKey = self::generateDerivedKey($key, $envelopeSalt);
+
             $plaintext = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt(
-                $ciphertext,
+                $ciphertextBody,
                 '',
                 $nonce,
                 $derivedKey
             );
+
             if ($plaintext === false) {
                 throw new DecryptionException('Failed to decrypt the data');
             }
 
-            // Return the decrypted data
             return $plaintext;
-        } catch (Exception $e) {
-            // Rethrow the exception
-            throw $e;
+        } catch (\Throwable $e) {
+            if ($e instanceof EncryptionException) {
+                throw $e;
+            }
+
+            throw new DecryptionException('Decryption failed', previous: $e);
         } finally {
-            // Wipe sensitive data
-            sodium_memzero($key);
-            sodium_memzero($salt);
-            sodium_memzero($derivedKey);
+            self::wipeString($derivedKey);
+            self::wipeString($decoded);
+            self::wipeString($nonce);
+            self::wipeString($ciphertextBody);
+            self::wipeString($envelopeSalt);
+        }
+    }
+
+    /**
+     * Decrypts legacy ciphertext payloads in hex format: [nonce][ciphertext+tag] (hex encoded).
+     *
+     * @param string $ciphertext Legacy hex ciphertext.
+     * @param string $key Encryption key.
+     * @param string $salt Salt value.
+     * @return string Unencrypted data.
+     *
+     * @throws DecryptionException If data decryption fails.
+     */
+    public static function decryptLegacyHex(string $ciphertext, string $key, string $salt): string
+    {
+        $derivedKey = null;
+        $decoded = null;
+        $nonce = null;
+        $ciphertextBody = null;
+
+        try {
+            $derivedKey = self::generateDerivedKey($key, $salt);
+            $decoded = sodium_hex2bin($ciphertext);
+
+            $minimumLength = self::NONCE_LENGTH + self::TAG_LENGTH;
+            if (strlen($decoded) < $minimumLength) {
+                throw new NonceLengthException('Decoded data is not the expected length');
+            }
+
+            $nonce = substr($decoded, 0, self::NONCE_LENGTH);
+            $ciphertextBody = substr($decoded, self::NONCE_LENGTH);
+            if (strlen($ciphertextBody) < self::TAG_LENGTH) {
+                throw new NonceLengthException('Ciphertext is not the expected length');
+            }
+
+            $plaintext = sodium_crypto_aead_xchacha20poly1305_ietf_decrypt(
+                $ciphertextBody,
+                '',
+                $nonce,
+                $derivedKey
+            );
+
+            if ($plaintext === false) {
+                throw new DecryptionException('Failed to decrypt the data');
+            }
+
+            return $plaintext;
+        } catch (\Throwable $e) {
+            if ($e instanceof EncryptionException) {
+                throw $e;
+            }
+
+            throw new DecryptionException('Legacy decryption failed', previous: $e);
+        } finally {
+            self::wipeString($derivedKey);
+            self::wipeString($decoded);
+            self::wipeString($nonce);
+            self::wipeString($ciphertextBody);
         }
     }
 
@@ -145,18 +231,11 @@ final class Cryptex
      *
      * @return string Random salt value of length SODIUM_CRYPTO_PWHASH_SALTBYTES.
      *
-     * @throws Exception If an error occurs while generating the salt value.
+     * @throws \Exception If an error occurs while generating the salt value.
      */
     public static function generateSalt(): string
     {
-        try {
-            $salt = random_bytes(self::SALT_LENGTH);
-            return $salt;
-        } catch (Exception $e) {
-            throw $e;
-        } finally {
-            sodium_memzero($salt);
-        }
+        return random_bytes(self::SALT_LENGTH);
     }
 
     /**
@@ -167,36 +246,45 @@ final class Cryptex
      * @return string Derived binary key.
      *
      * @throws SaltLengthException If the salt is not the expected length.
-     * @throws Exception If an error occurs while generating the derived binary key.
+     * @throws EncryptionException If key derivation fails.
      */
     private static function generateDerivedKey(string $key, string $salt): string
     {
-        try {
-            // Salt length requirement check
-            if (strlen($salt) !== self::SALT_LENGTH) {
-                throw new SaltLengthException('Salt is not the expected length');
-            }
+        if (strlen($salt) !== self::SALT_LENGTH) {
+            throw new SaltLengthException('Salt is not the expected length');
+        }
 
-            // Generate the derived binary key
-            $derivedKey = sodium_crypto_pwhash(
-                SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_KEYBYTES,
+        try {
+            return sodium_crypto_pwhash(
+                \SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_KEYBYTES,
                 $key,
                 $salt,
-                SODIUM_CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
-                SODIUM_CRYPTO_PWHASH_MEMLIMIT_INTERACTIVE,
-                SODIUM_CRYPTO_PWHASH_ALG_ARGON2ID13
+                \SODIUM_CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
+                \SODIUM_CRYPTO_PWHASH_MEMLIMIT_INTERACTIVE,
+                \SODIUM_CRYPTO_PWHASH_ALG_ARGON2ID13
             );
+        } catch (\Throwable $e) {
+            throw new EncryptionException('Failed to derive encryption key', previous: $e);
+        }
+    }
 
-            // Return the derived binary key
-            return $derivedKey;
-        } catch (Exception $e) {
-            // Rethrow the exception
-            throw $e;
-        } finally {
-            // Wipe sensitive data
-            sodium_memzero($key);
-            sodium_memzero($salt);
-            sodium_memzero($derivedKey);
+    /**
+     * Heuristic detection for the legacy hex payload format.
+     */
+    private static function looksLikeLegacyHex(string $ciphertext): bool
+    {
+        return $ciphertext !== ''
+            && (strlen($ciphertext) % 2 === 0)
+            && ctype_xdigit($ciphertext);
+    }
+
+    /**
+     * Securely wipe a string when possible.
+     */
+    private static function wipeString(mixed &$value): void
+    {
+        if (isset($value) && is_string($value)) {
+            sodium_memzero($value);
         }
     }
 }
@@ -206,12 +294,6 @@ final class Cryptex
  * Custom exception class for encryption errors.
  */
 class EncryptionException extends \Exception {}
-
-/**
- * Class EncodingException
- * Custom exception class for encoding errors.
- */
-class EncodingException extends EncryptionException {}
 
 /**
  * Class NonceLengthException
--- tests/CryptexTest.php
+++ tests/CryptexTest.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace cryptex;
 
 use PHPUnit\Framework\TestCase;
@@ -6,150 +9,101 @@ use ReflectionClass;
 
 /**
  * CryptexTest performs unit testing for the Cryptex class.
- *
- * @category Tests
- * @package Cryptex
- * @author Michael Mawhinney
- * @copyright 2023
- * @license https://opensource.org/licenses/MIT/ MIT
- * @version 4.0.0
  */
 final class CryptexTest extends TestCase
 {
-    /**
-     * @var string Key for encryption and decryption tests.
-     */
-    private $key;
+    private string $key;
+    private string $salt;
+    private int $saltLength;
+    private int $nonceLength;
+    private int $tagLength;
+    private string $plaintext;
+    private string $ciphertext;
 
-    /**
-     * @var string Salt for encryption and decryption tests.
-     */
-    private $salt;
-
-    /**
-     * @var int Length of the salt value.
-     */
-    private $saltLength;
-
-    /**
-     * @var string Plaintext string for encryption and decryption tests.
-     */
-    private $plaintext;
-
-    /**
-     * @var string Ciphertext string for encryption and decryption tests.
-     */
-    private $ciphertext;
-
-    /**
-     * Sets up each test by initializing required variables and generating encrypted data for decryption tests.
-     */
     protected function setUp(): void
     {
-        // Access the private vars with reflection
         $reflection = new ReflectionClass(Cryptex::class);
         $this->saltLength = $reflection->getReflectionConstant('SALT_LENGTH')->getValue();
+        $this->nonceLength = $reflection->getReflectionConstant('NONCE_LENGTH')->getValue();
+        $this->tagLength = $reflection->getReflectionConstant('TAG_LENGTH')->getValue();
 
-        // Set the remaining vars
         $this->key = '1-2-3-4-5';
         $this->salt = Cryptex::generateSalt();
         $this->plaintext = "You're a certified prince.";
         $this->ciphertext = Cryptex::encrypt($this->plaintext, $this->key, $this->salt);
     }
 
-    /**
-     * Tests the `generateSalt()` method.
-     */
     public function testGenerateSalt(): void
     {
-        $this->assertIsInt($this->saltLength);
         $this->assertIsString($this->salt);
-        $this->assertEquals($this->saltLength, strlen($this->salt));
+        $this->assertSame($this->saltLength, strlen($this->salt));
     }
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with valid inputs.
-     */
-    public function testEncryptDecrypt(): void
+    public function testEncryptDecryptRoundTripV1Envelope(): void
     {
         $this->assertIsString($this->ciphertext);
-        $this->assertNotEquals($this->plaintext, $this->ciphertext);
+        $this->assertNotSame($this->plaintext, $this->ciphertext);
 
-        $decrypted = Cryptex::decrypt($this->ciphertext, $this->key, $this->salt);
-        $this->assertEquals($this->plaintext, $decrypted);
+        $decoded = sodium_base642bin($this->ciphertext, \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING, '');
+
+        $this->assertSame(1, ord($decoded[0]));
+        $this->assertSame($this->salt, substr($decoded, 1, $this->saltLength));
+
+        $decrypted = Cryptex::decrypt($this->ciphertext, $this->key, random_bytes($this->saltLength));
+        $this->assertSame($this->plaintext, $decrypted);
     }
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with invalid input values.
-     */
-    public function testEncryptDecryptWithInvalidInput(): void
+    public function testTamperedPayloadFails(): void
     {
-        $this->expectException(\TypeError::class);
+        $decoded = sodium_base642bin($this->ciphertext, \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING, '');
+        $decoded[strlen($decoded) - 1] = $decoded[strlen($decoded) - 1] ^ "\x01";
+        $tampered = sodium_bin2base64($decoded, \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING);
 
-        $invalidInputs = [null, '', [], new \stdClass(), true];
-        foreach ($invalidInputs as $invalidInput) {
-            Cryptex::encrypt($invalidInput, $this->key, $this->salt);
-            Cryptex::decrypt($invalidInput, $this->key, $this->salt);
-        }
+        $this->expectException(DecryptionException::class);
+        Cryptex::decrypt($tampered, $this->key, $this->salt);
     }
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with invalid key or salt.
-     */
-    public function testEncryptDecryptWithInvalidKeyOrSalt(): void
+    public function testWrongKeyFailsDecryption(): void
     {
-        $this->expectException(\TypeError::class);
-        $invalidInputs = [null, '', [], new \stdClass(), true, 'invalid'];
-        foreach ($invalidInputs as $invalidInput) {
-            Cryptex::encrypt($this->ciphertext, $invalidInput, $this->salt);
-            Cryptex::decrypt($this->ciphertext, $invalidInput, $this->salt);
-            Cryptex::encrypt($this->ciphertext, $this->key, $invalidInput);
-            Cryptex::decrypt($this->ciphertext, $this->key, $invalidInput);
-        }
+        $this->expectException(DecryptionException::class);
+        Cryptex::decrypt($this->ciphertext, 'definitely-not-the-right-key', $this->salt);
     }
 
-    /**
-     * Tests the `decrypt()` method with invalid ciphertext.
-     */
-    public function testDecryptWithInvalidCiphertext(): void
+    public function testWrongSaltLengthOnEncryptThrows(): void
     {
-        $this->expectException(\Exception::class);
-        Cryptex::decrypt('invalid ciphertext', $this->key, $this->salt);
+        $this->expectException(SaltLengthException::class);
+        Cryptex::encrypt($this->plaintext, $this->key, 'short');
     }
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with a large plaintext string.
-     */
-    public function testLargePlaintext(): void
+    public function testMalformedPayloadThrows(): void
     {
-        $largePlaintext = str_repeat('x', 1000000); // 1 million characters
-        $ciphertext = Cryptex::encrypt($largePlaintext, $this->key, $this->salt);
-        $decrypted = Cryptex::decrypt($ciphertext, $this->key, $this->salt);
+        $minimumLength = 1 + $this->saltLength + $this->nonceLength + $this->tagLength;
+        $shortEnvelope = str_repeat("\x00", $minimumLength - 1);
+        $payload = sodium_bin2base64($shortEnvelope, \SODIUM_BASE64_VARIANT_URLSAFE_NO_PADDING);
 
-        $this->assertEquals($largePlaintext, $decrypted);
+        $this->expectException(NonceLengthException::class);
+        Cryptex::decrypt($payload, $this->key, $this->salt);
     }
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with a plaintext string that contains non-alphanumeric characters.
-     */
-    public function testNonAlphanumericCharacters(): void
+    public function testLegacyHexDecryptStillSupported(): void
     {
-        $nonAlphanumericPlaintext = '!@#$%^&*(){}[]:;"<>,.?/~`|-_=+\\';
-        $ciphertext = Cryptex::encrypt($nonAlphanumericPlaintext, $this->key, $this->salt);
-        $decrypted = Cryptex::decrypt($ciphertext, $this->key, $this->salt);
+        $derivedKey = sodium_crypto_pwhash(
+            \SODIUM_CRYPTO_AEAD_XCHACHA20POLY1305_IETF_KEYBYTES,
+            $this->key,
+            $this->salt,
+            \SODIUM_CRYPTO_PWHASH_OPSLIMIT_INTERACTIVE,
+            \SODIUM_CRYPTO_PWHASH_MEMLIMIT_INTERACTIVE,
+            \SODIUM_CRYPTO_PWHASH_ALG_ARGON2ID13
+        );
 
-        $this->assertEquals($nonAlphanumericPlaintext, $decrypted);
-    }
+        $nonce = random_bytes($this->nonceLength);
+        $encrypted = sodium_crypto_aead_xchacha20poly1305_ietf_encrypt($this->plaintext, '', $nonce, $derivedKey);
+        $legacyPayload = sodium_bin2hex($nonce . $encrypted);
 
-    /**
-     * Tests the `encrypt()` and `decrypt()` methods with a key that contains non-alphanumeric characters.
-     */
-    public function testKeyNonAlphanumericCharacters(): void
-    {
-        $nonAlphanumericKey = '!@#$%^&*(){}[]:;"<>,.?/~`|-_=+\\';
-        $ciphertext = Cryptex::encrypt($this->plaintext, $nonAlphanumericKey, $this->salt);
-        $decrypted = Cryptex::decrypt($ciphertext, $nonAlphanumericKey, $this->salt);
+        $decrypted = Cryptex::decrypt($legacyPayload, $this->key, $this->salt);
+        $this->assertSame($this->plaintext, $decrypted);
 
-        $this->assertEquals($this->plaintext, $decrypted);
+        sodium_memzero($derivedKey);
+        sodium_memzero($nonce);
     }
 }
